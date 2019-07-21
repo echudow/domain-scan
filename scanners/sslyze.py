378310f84eb4f8c4bf11d1d47ee449dfd7bf72b9
echudow <eric.b.chudow.civ@mail.mil>
2019-07-21 07:30:13 -0400

    Fixed STARTTLS header

--- scanners/sslyze.py
+++ scanners/sslyze.py
@@ -256,7 +256,7 @@ def to_rows(data):
 headers = [
     "Scanned Hostname",
     "Scanned Port",
-    "scanned for STARTTLS SMTP",
+    "Scanned for STARTTLS SMTP",
     "SSLv2", "SSLv3", "TLSv1.0", "TLSv1.1", "TLSv1.2", "TLSv1.3",
 
     "Any Forward Secrecy", "All Forward Secrecy",
